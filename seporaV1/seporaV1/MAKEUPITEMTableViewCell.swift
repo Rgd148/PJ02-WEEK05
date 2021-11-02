b36c408e75f9eb7e7109b5540deda94e69b60944
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-11-02 16:48:19 +0300

    Adding cells

---
+++ seporaV1/seporaV1/MAKEUPITEMTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MAKEUPITEMTableViewCell.swift
+//  seporaV1
+//
+//  Created by Raghad Alahmadi on 27/03/1443 AH.
+//
+
+import UIKit
+
+class MAKEUPITEMTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
