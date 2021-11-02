54b8c9c84edecef6d2fa28b0827f975a428af54e
Raghad Alahmadi <Raghad.Eid.med.s@tuwaiq.edu.sa>
2021-11-02 20:11:48 +0300

    add cellForRowAt function

--- seporaV1/seporaV1/MAKEUPTableVC.swift
+++ seporaV1/seporaV1/MAKEUPTableVC.swift
@@ -49,15 +49,31 @@ class MAKEUPTableVC: UITableViewController {
         }
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        
+        
+        if indexPath.section == 0 {
+        let cellBanner = tableView.dequeueReusableCell(withIdentifier: "MBannerID") as! MAKEUPBANNERTableViewCell
+        
+            return cellBanner
+            
+        }
+        else {
+   let cellMProduct = tableView.dequeueReusableCell(withIdentifier: "MItemID") as! MAKEUPITEMTableViewCell
+            
+            
+            cellMProduct.MItemName.text = Mproduct[indexPath.row].Mname
+            
+            cellMProduct.MItemDec.text = Mproduct[indexPath.row].Mdec
+            
+            cellMProduct.MItemCost.text = String(Mproduct[indexPath.row].Mcost)
+            cellMProduct.MItemImage.image = Mproduct[indexPath.row].Mimage
+            
+            return cellMProduct
+        }
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -67,17 +83,19 @@ class MAKEUPTableVC: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            Mproduct.remove(at: indexPath.row)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
+        tableView.reloadData()
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
